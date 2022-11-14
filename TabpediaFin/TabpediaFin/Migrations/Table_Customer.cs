24bd9f728601b4a1708f631654304225f435906e
fanus99 <fajaryunus99@gmail.com>
2022-11-14 16:49:32 +0700

    migration master vendor cust CoA

---
+++ TabpediaFin/TabpediaFin/Migrations/Table_Customer.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+
+namespace TabpediaFin.Migrations
+{
+    [Migration(202211141605)]
+    public class Table_Customer : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Master_Customer");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Master_Customer")
+                .WithColumn("Id").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("CustName").AsString(250).NotNullable()
+                .WithColumn("Address").AsString(250).NotNullable()
+                .WithColumn("Email").AsString(250).NotNullable()
+                .WithColumn("Phone").AsBoolean().NotNullable().WithDefaultValue(true)
+                .WithColumn("CreatedUid").AsInt32().NotNullable()
+                .WithColumn("CreatedUtc").AsDateTime().NotNullable()
+                .WithColumn("UpdatedUid").AsInt32().Nullable()
+                .WithColumn("UpdatedUtc").AsDateTime().Nullable();
+        }
+    }
+}
---
+++ TabpediaFin/TabpediaFin/Migrations/Table_vendor.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+
+namespace TabpediaFin.Migrations
+{
+    [Migration(202211141600)]
+    public class Table_vendor:Migration
+    {
+        
+        public override void Up()
+        {
+            Create.Table("Master_Vendor")
+                .WithColumn("vendor_id").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("badan_usaha").AsString(250).NotNullable()
+                .WithColumn("npwp_badan").AsString(250).NotNullable()
+                .WithColumn("rekening").AsString(250).NotNullable()
+                .WithColumn("bank").AsString(250).NotNullable()
+                .WithColumn("nama_direktur").AsString(250).NotNullable()
+                .WithColumn("email").AsString(250).NotNullable()
+                .WithColumn("telp").AsString(250).NotNullable()
+                .WithColumn("alamat").AsString(250).NotNullable()
+                .WithColumn("kop").AsString(250).NotNullable()
+                .WithColumn("akta_notaris").AsString(250).NotNullable()
+                .WithColumn("CreatedUid").AsInt32().NotNullable()
+                .WithColumn("CreatedUtc").AsDateTime().NotNullable()
+                .WithColumn("UpdatedUid").AsInt32().Nullable()
+                .WithColumn("UpdatedUtc").AsDateTime().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Master_Vendor");
+        }
+    }
+}
