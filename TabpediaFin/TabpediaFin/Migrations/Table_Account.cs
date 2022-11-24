fe3074250bf4cbf3360c2817c73f7124e1c584ce
hansench <hansen.chandrawinata@gmail.com>
2022-11-24 19:06:47 +0700

    Create Table_Account.cs

---
+++ TabpediaFin/TabpediaFin/Migrations/Table_Account.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace TabpediaFin.Migrations;
+
+[Migration(202211171107)]
+public class Table_Account : Migration
+{
+    public override void Down()
+    {
+
+    }
+
+    public override void Up()
+    {
+        Create.Table("Tag")
+            .WithColumn("Id").AsInt32().NotNullable().Identity().PrimaryKey()
+            .WithColumn("TenantId").AsInt32().NotNullable()
+            .WithColumn("Name").AsString(250).NotNullable()
+            .WithColumn("Description").AsString(250).Nullable()
+            .WithColumn("CreatedUid").AsInt32().NotNullable()
+            .WithColumn("CreatedUtc").AsDateTime().NotNullable()
+            .WithColumn("UpdatedUid").AsInt32().Nullable()
+            .WithColumn("UpdatedUtc").AsDateTime().Nullable();
+
+    }
+}
