abab3cf4bf1d8b2a34d642e6e0f96ca92ab82a99
hansench <hansen.chandrawinata@gmail.com>
2022-11-24 19:41:43 +0700

    Migrations GL

--- TabpediaFin/TabpediaFin/Migrations/Table_Account.cs
+++ TabpediaFin/TabpediaFin/Migrations/Table_Account.cs
@@ -1,26 +1,27 @@
-﻿using FluentMigrator;
+﻿//using FluentMigrator;
 
-namespace TabpediaFin.Migrations;
+//namespace TabpediaFin.Migrations;
 
-[Migration(202211171107)]
-public class Table_Account : Migration
-{
-    public override void Down()
-    {
+//[Migration(202211171107)]
+//public class Table_Account : Migration
+//{
+//    public override void Down()
+//    {
 
-    }
+//    }
 
-    public override void Up()
-    {
-        Create.Table("Tag")
-            .WithColumn("Id").AsInt32().NotNullable().Identity().PrimaryKey()
-            .WithColumn("TenantId").AsInt32().NotNullable()
-            .WithColumn("Name").AsString(250).NotNullable()
-            .WithColumn("Description").AsString(250).Nullable()
-            .WithColumn("CreatedUid").AsInt32().NotNullable()
-            .WithColumn("CreatedUtc").AsDateTime().NotNullable()
-            .WithColumn("UpdatedUid").AsInt32().Nullable()
-            .WithColumn("UpdatedUtc").AsDateTime().Nullable();
+//    public override void Up()
+//    {
+//        //Create.Table("Table_Account")
+//        //    .WithColumn("Id").AsInt32().NotNullable().Identity().PrimaryKey()
+//        //    .WithColumn("TenantId").AsInt32().NotNullable()
+//        //    .WithColumn("Code").AsString(250).NotNullable()
+//        //    .WithColumn("Name").AsString(250).NotNullable()
+//        //    .WithColumn("Description").AsString(250).Nullable()
+//        //    .WithColumn("CreatedUid").AsInt32().NotNullable()
+//        //    .WithColumn("CreatedUtc").AsDateTime().NotNullable()
+//        //    .WithColumn("UpdatedUid").AsInt32().Nullable()
+//        //    .WithColumn("UpdatedUtc").AsDateTime().Nullable();
 
-    }
-}
+//    }
+//}
