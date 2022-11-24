9e37dd7e5568f841c1143e49bba21598b83e825d
fanus99 <fajaryunus99@gmail.com>
2022-11-24 08:51:39 +0700

    exportimport contact with npoi

--- TabpediaFin/TabpediaFin/Handler/ExportImportContactHandler/ContactReadFileHandler.cs
+++ TabpediaFin/TabpediaFin/Handler/ExportImportContactHandler/ContactReadFileHandler.cs
@@ -1,6 +1,5 @@
 ﻿using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
-//using OfficeOpenXml;
 
 namespace TabpediaFin.Handler.ExportImportContactHandler
 {
@@ -20,20 +19,6 @@ namespace TabpediaFin.Handler.ExportImportContactHandler
 
             try
             {
-                if (request.formFile == null || request.formFile.Length <= 0)
-                {
-                    result.IsOk = false;
-                    result.ErrorMessage = "file is empty";
-                    return result;
-                }
-
-                if (!Path.GetExtension(request.formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
-                {
-                    result.IsOk = false;
-                    result.ErrorMessage = "Not Support file extension";
-                    return result;
-                }
-
                 if (request.formFile == null || request.formFile.Length <= 0)
                 {
                     result.IsOk = false;
@@ -79,7 +64,7 @@ namespace TabpediaFin.Handler.ExportImportContactHandler
                                 Fax = sheetRow.GetCell(6).StringCellValue,
                                 Website = sheetRow.GetCell(7).StringCellValue,
                                 Npwp = sheetRow.GetCell(8).StringCellValue,
-                                //GroupId = int.Parse(sheetRow.GetCell(9).StringCellValue),
+                                GroupId = int.Parse(sheetRow.GetCell(9).StringCellValue),
                                 IsCustomer = bool.Parse(sheetRow.GetCell(10).StringCellValue),
                                 IsVendor = bool.Parse(sheetRow.GetCell(11).StringCellValue),
                                 IsEmployee = bool.Parse(sheetRow.GetCell(12).StringCellValue),
@@ -96,48 +81,6 @@ namespace TabpediaFin.Handler.ExportImportContactHandler
                 result.ErrorMessage = string.Empty;
                 result.List = list?.AsList() ?? new List<ContactReadFileListDto>();
                 result.RecordCount = recordCount;
-
-                //var list = new List<ContactReadFileListDto>();
-
-                //using (var stream = new MemoryStream())
-                //{
-                //    await request.formFile.CopyToAsync(stream, cancellationToken);
-
-                //    using (var package = new ExcelPackage(stream))
-                //    {
-                //        ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-                //        var rowCount = worksheet.Dimension.Rows;
-
-                //        for (int row = 2; row <= rowCount; row++)
-                //        {
-                //            list.Add(new ContactReadFileListDto
-                //            {
-                //                Name = worksheet.Cells[row, 1].Value.ToString().Trim(),
-                //                Address = worksheet.Cells[row, 2].Value.ToString().Trim(),
-                //                CityName = worksheet.Cells[row, 3].Value.ToString().Trim(),
-                //                PostalCode = worksheet.Cells[row, 4].Value.ToString().Trim(),
-                //                Email = worksheet.Cells[row, 5].Value.ToString().Trim(),
-                //                Phone = worksheet.Cells[row, 6].Value.ToString().Trim(),
-                //                Fax = worksheet.Cells[row, 7].Value.ToString().Trim(),
-                //                Website = worksheet.Cells[row, 8].Value.ToString().Trim(),
-                //                Npwp = worksheet.Cells[row, 9].Value.ToString().Trim(),
-                //                GroupId = int.Parse(worksheet.Cells[row, 10].Value.ToString().Trim()),
-                //                IsCustomer = bool.Parse(worksheet.Cells[row, 11].Value.ToString().Trim()),
-                //                IsVendor = bool.Parse(worksheet.Cells[row, 12].Value.ToString().Trim()),
-                //                IsEmployee = bool.Parse(worksheet.Cells[row, 13].Value.ToString().Trim()),
-                //                IsOther = bool.Parse(worksheet.Cells[row, 14].Value.ToString().Trim()),
-                //                Notes = worksheet.Cells[row, 15].Value.ToString().Trim(),
-                //            });
-                //        }
-                //    }
-
-                //    int recordCount = list.Count();
-
-                //    result.IsOk = true;
-                //    result.ErrorMessage = string.Empty;
-                //    result.List = list?.AsList() ?? new List<ContactReadFileListDto>();
-                //    result.RecordCount = recordCount;
-                //}
             }
             catch (Exception ex)
             {
