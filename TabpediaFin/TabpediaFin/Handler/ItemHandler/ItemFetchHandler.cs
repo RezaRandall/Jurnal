fdd1d45c62330f7d2eb872cd94500c396fbb91a9
Dwi Reza Irawan <dwirezairawan07@gmail.com>
2022-12-20 10:34:57 +0700

    display itemUnitMesaure, itemAttachment, itemItemCategory when get data by id in item module

--- TabpediaFin/TabpediaFin/Handler/ItemHandler/ItemFetchHandler.cs
+++ TabpediaFin/TabpediaFin/Handler/ItemHandler/ItemFetchHandler.cs
@@ -1,4 +1,6 @@
-﻿using TabpediaFin.Handler.ItemItemCategoryHandler;
+﻿using TabpediaFin.Domain;
+using TabpediaFin.Handler.ContactHandler;
+using TabpediaFin.Handler.ItemItemCategoryHandler;
 using TabpediaFin.Handler.ItemUnitMeasureHandler;
 
 namespace TabpediaFin.Handler.Item
@@ -22,19 +24,63 @@ namespace TabpediaFin.Handler.Item
             {
                 using (var cn = _dbManager.CreateConnection())
                 {
-                    var row = await cn.FetchAsync<ItemDto>(request.Id, _currentUser);
-                    if (row == null)
+                    var sql = @"SELECT * FROM ""Item"" WHERE ""TenantId"" = @TenantId AND ""Id"" = @Id " ;
+
+                    var parameters = new DynamicParameters();
+                    parameters.Add("TenantId", _currentUser.TenantId);
+                    parameters.Add("Id", request.Id);
+                    var result = await cn.QueryFirstOrDefaultAsync<ItemDto>(sql, parameters);
+
+                    if (result != null)
                     {
-                        response.IsOk = false;
-                        response.Row = row;
-                        response.ErrorMessage = "Data not found";
+                        var sqlItemItemCategory = @"SELECT iic.""Id""
+                                                ,iic.""ItemId""
+                                                ,iic.""ItemCategoryId""
+                                                 FROM ""ItemItemCategory"" iic
+                                                 LEFT JOIN ""Item"" i ON iic.""ItemId"" = i.""Id"" 
+                                                 WHERE i.""TenantId"" = @TenantId AND i.""Id"" = @IdItem ";
+
+                        var parametersub = new DynamicParameters();
+                        parametersub.Add("TenantId", _currentUser.TenantId);
+                        parametersub.Add("IdItem", request.Id);
+
+                        List<ItemItemCategoryFetchDto> resultItemItemCategory;
+                        resultItemItemCategory = (await cn.QueryAsync<ItemItemCategoryFetchDto>(sqlItemItemCategory, parametersub).ConfigureAwait(false)).ToList();
+                        result.ItemItemCategoryList = resultItemItemCategory;
+
+                        var sqlItemUnitMeasure = @"SELECT ium.""Id""
+                                        ,ium.""ItemId""
+                                        ,ium.""UnitMeasureId""
+                                        ,ium.""UnitConversion""
+                                        ,ium.""Cost""
+                                        ,ium.""Price""
+                                        ,ium.""Notes""
+                                        FROM ""ItemUnitMeasure"" ium 
+                                        INNER JOIN ""Item"" i ON ium.""ItemId"" = i.""Id""
+                                        WHERE i.""TenantId"" = @TenantId AND i.""Id"" = @IdItem";
+
+                        List<ItemUnitMeasureFetchDto> resultItemUnitMeasure;
+                        resultItemUnitMeasure = (await cn.QueryAsync<ItemUnitMeasureFetchDto>(sqlItemUnitMeasure, parametersub).ConfigureAwait(false)).ToList();
+                        result.ItemUnitMeasureList = resultItemUnitMeasure;
+
+                        var sqlAttachment = @"SELECT ia.""Id""
+                                        , ia.""FileName""
+                                        , ia.""FileUrl""
+                                        , ia.""Extension""
+                                        , ia.""FileSize""
+                                        , ia.""ItemId"" 
+                                        FROM ""ItemAttachment"" ia
+                                        INNER JOIN ""Item"" i ON ia.""ItemId"" = i.""Id"" 
+                                        WHERE i.""TenantId"" = @TenantId AND i.""Id"" = @IdItem ";
+                        List<ItemFetchAttachment> resultItemAttachment;
+                        resultItemAttachment = (await cn.QueryAsync<ItemFetchAttachment>(sqlAttachment, parametersub).ConfigureAwait(false)).ToList();
+                        result.ItemAttachmentList = resultItemAttachment;
+
                     }
-                    else 
-                    {
-                        response.IsOk = true;
-                        response.Row = row;
-                        response.ErrorMessage = string.Empty;
-                    }
+
+                    response.IsOk = true;
+                    response.Row = result;
+                    response.ErrorMessage = string.Empty;
                 }
             }
             catch (Exception ex)
@@ -67,7 +113,6 @@ public class ItemDto : BaseDto
     public bool IsArchived { get; set; } = true;
     public string ImageFileName { get; set; } = string.Empty;
     public string Notes { get; set; } = string.Empty;
-
     public List<ItemItemCategoryFetchDto> ItemItemCategoryList { get; set; }
     public List<ItemUnitMeasureFetchDto> ItemUnitMeasureList { get; set; }
     public List<ItemFetchAttachment> ItemAttachmentList { get; set; }
