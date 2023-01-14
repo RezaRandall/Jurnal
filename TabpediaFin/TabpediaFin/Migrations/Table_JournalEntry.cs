12f11c2a85cc67207aad46d3f1e0c31181801d18
fanus99 <fajaryunus99@gmail.com>
2023-01-14 10:07:56 +0700

    add migration journal entry

---
+++ TabpediaFin/TabpediaFin/Migrations/Table_JournalEntry.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+
+namespace TabpediaFin.Migrations;
+
+[Migration(202301011018)]
+public class Table_JournalEntry : Migration
+{
+    public override void Down()
+    {
+
+    }
+
+    public override void Up()
+    {
+        Create.Table("JournalEntry")
+            .WithColumn("Id").AsInt32().NotNullable().Identity().PrimaryKey()
+            .WithColumn("TenantId").AsInt32().NotNullable()
+            .WithColumn("TransDate").AsDateTime().NotNullable()
+            .WithColumn("TransCode").AsString(250).NotNullable()
+            .WithColumn("Memo").AsString(250).Nullable()
+            .WithColumn("Notes").AsString(250).Nullable()
+            .WithColumn("CreatedUid").AsInt32().NotNullable()
+            .WithColumn("CreatedUtc").AsDateTime().NotNullable()
+            .WithColumn("UpdatedUid").AsInt32().Nullable()
+            .WithColumn("UpdatedUtc").AsDateTime().Nullable();
+    }
+}
---
+++ TabpediaFin/TabpediaFin/Migrations/Table_JournalEntryItem.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace TabpediaFin.Migrations;
+
+[Migration(202301011016)]
+public class Table_JournalEntryItem : Migration
+{
+    public override void Down()
+    {
+
+    }
+
+    public override void Up()
+    {
+        Create.Table("JournalEntryItem")
+            .WithColumn("Id").AsInt32().NotNullable().Identity().PrimaryKey()
+            .WithColumn("TenantId").AsInt32().NotNullable()
+            .WithColumn("TransId").AsInt32().NotNullable()
+            .WithColumn("AccountId").AsInt32().NotNullable()
+            .WithColumn("ItemId").AsInt32().NotNullable()
+            .WithColumn("Description").AsString(255).Nullable()
+            .WithColumn("Debit").AsDecimal(18, 2).NotNullable().WithDefaultValue(0)
+            .WithColumn("Credit").AsDecimal(18, 2).NotNullable().WithDefaultValue(0)
+            .WithColumn("CreatedUid").AsInt32().NotNullable()
+            .WithColumn("CreatedUtc").AsDateTime().NotNullable()
+            .WithColumn("UpdatedUid").AsInt32().Nullable()
+            .WithColumn("UpdatedUtc").AsDateTime().Nullable();
+    }
+}
